46ec191bfa6bb9bc6036760695e0af898b8749d6
LQ <317006185@qq.com>
2022-03-31 21:06:44 +0800

    time fangfa

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from datetime import datetime
 # Create your views here.
+
+
+def timestr(request):
+    time_str = datetime.now().strftime("%Y-%m-%d-%X")
+    return render(request,'index.html',{'now':time_str})
