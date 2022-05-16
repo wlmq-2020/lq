489562f4d21dfd8de4fe1e2cc1733a7a299afa6c
liqi <317006185@qq.com>
2022-05-16 22:30:05 +0800

    uuu

--- Sflask/__init__.py
+++ Sflask/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_session import RedisSessionInterface
+from redis import Redis
 
 from .view import account
 from .view import home
@@ -8,6 +10,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object('settings.DevelopmentConfig')
     app.secret_key = 'jbhjkhjbgjhvghf54535ertdtdfgfhj'
+    app.session_interface = RedisSessionInterface(redis=Redis(host="localhost", port=6379), key_prefix='sssp')
     app.debug = True
     # 绑定蓝图
     app.register_blueprint(account.account)
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from redis import Redis
 from flask_session import RedisSessionInterface
 
 app = create_app()
-app.session_interface = RedisSessionInterface(redis=Redis(host="localhost",port=6379), key_prefix='sssp')
+
 
 if __name__ == '__main__':
     app.run()
